169005f88ed48ae0ee73babcb5c3d340d3397e9b
WendyWu <92006997+wendy61019@users.noreply.github.com>
2026-07-23 21:22:17 +0800

    Add files via upload

--- app_streamlit.py
+++ app_streamlit.py
@@ -167,7 +167,7 @@ def main():
             #顯示原始照片
             st.image(uploaded_file, caption="已上傳原始照片")
             #顯示預處理照片
-            st.image(processed_img, caption="OpenCV預處理結果")
+            st.expander(processed_img, caption="OpenCV預處理結果")
             #按鈕觸發辨識
             if st.button("🚀 進行車牌辨識", type="primary"):
                 car_plate = extract_plate_text(processed_img)
